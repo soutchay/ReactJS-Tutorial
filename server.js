3c71d29cd2f5eb5542d07f99f8b5f6ceabec55fc
soutchay <chungvus@gmail.com>
2015-03-17 16:05:01 -0700

    setup root index page

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-server.js
+var express = require('express');
+var app = express();
+
+var router = express.Router();
+
+//set up static content
+app.use(express.static(__dirname + '/views'));
+
+
+
+//Set up route for root page
+router.route('/')
+  .get(function(req, res){
+    res.status(200).render("index.html");
+  });
+
+port = process.env.PORT || 1234;
+app.listen(port);
+console.log("Listening on port", port);
