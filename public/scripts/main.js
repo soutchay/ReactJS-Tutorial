3722e48e919c2bee6eea596e96499b3b6f7dbaa6
soutchay <chungvus@gmail.com>
2015-03-17 17:28:12 -0700

    add commentform, add commentlist

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,41 @@
 //React stuff goes here
-//make a comment box
 
+//make a comment box
 var CommentBox = React.createClass({
   render: function(){
     return (
       <div className="commentBox">
-        Some Comment Box Here
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
       );
   }
 });
 
+var CommentList = React.createClass({
+  render: function(){
+    return (
+    <div className="commentList">
+      This be a Comment List
+    </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    return (
+    <div className="commentForm">
+      Comment Form would go here
+    </div>
+    );
+  }
+});
+
+
+
+//calls render property to instantiate component and fires function to append CommentBox to id="content"
 React.render(
   <CommentBox />,
   document.getElementById('content')
