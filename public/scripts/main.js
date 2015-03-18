69b08ab5f8207d2ec6e46c2722fc4d1470a5a2af
soutchay <chungvus@gmail.com>
2015-03-17 17:55:48 -0700

    add showdownjs

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,10 @@
 //React stuff goes here
 
+//allow use of ShowdownJS
+var converter = new Showdown.converter();
+
 //make a comment box
+
 var CommentBox = React.createClass({
   render: function(){
     return (
@@ -15,9 +19,12 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function(){
+    //Add comment to comment list which passes author name
     return (
     <div className="commentList">
       This be a Comment List
+      <Comment author="Jon Jon">This is one comment</Comment>
+      <Comment author="Ryan Ryan">This is another comment</Comment>
     </div>
     );
   }
@@ -33,6 +40,20 @@ var CommentForm = React.createClass({
   }
 });
 
+//Set up props for comment
+//Depends on data being passed on from the parent
+var Comment = React.createClass({
+  render: function(){
+    return (
+    <div className="comment">
+      <h2 className="commentAuthor">
+        {this.props.author} (This is being passed from the CommentList)
+      </h2>
+      {converter.makeHtml(this.props.children.toString())}
+    </div>
+    )
+  }
+});
 
 
 //calls render property to instantiate component and fires function to append CommentBox to id="content"
