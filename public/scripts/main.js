92c6f00e791c50837541522971f77b214f7fe35f
soutchay <chungvus@gmail.com>
2015-03-17 17:12:59 -0700

    first react component

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,17 @@
 //React stuff goes here
+//make a comment box
+
+var CommentBox = React.createClass({
+  render: function(){
+    return (
+      <div className="commentBox">
+        Some Comment Box Here
+      </div>
+      );
+  }
+});
+
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+  )
